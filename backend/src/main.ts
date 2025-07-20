43513499f74747c45ea80d05127fa298844fd393
Alan Galavis <alangalavisd@gmail.com>
2025-07-19 19:42:26 -0500

    fix: Cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+
   console.log(
     `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
   );
